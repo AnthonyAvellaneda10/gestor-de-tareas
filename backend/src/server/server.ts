ee70146e95121c332f634bf059cb02543a333f58
Anthony <anthony_cristhian7@hotmail.com>
2025-02-21 00:10:10 -0500

    chore(deps): update dependencies and configuration

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import router from '../routes/routes';
+import router from '../routes/tasks.routes';
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+app.use(express.json()); // Para parsear el cuerpo de las solicitudes JSON
 
 app.use('/', router);
-
 
 export default app;
