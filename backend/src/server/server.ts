d45197a145efd2cd47dae6f431f965f518b01848
Anthony <anthony_cristhian7@hotmail.com>
2025-02-22 17:04:36 -0500

    chore(backend/server): update server configuration and startup logic

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -2,14 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import router from '../routes/tasks.routes';
+import { errorHandler } from '../middlewares/errorHandler'; // Importar el middleware de errores
+import { setupSwagger } from '../swagger/swagger'; // Importar la configuración de Swagger
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json()); // Para parsear el cuerpo de las solicitudes JSON
+app.use(express.json());
 
 app.use('/', router);
+
+setupSwagger(app);
+
+app.use(errorHandler);
 
 export default app;
