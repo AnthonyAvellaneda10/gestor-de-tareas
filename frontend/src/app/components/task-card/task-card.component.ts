438c4fe0ad945205df83de1fe3001609f7cf1495
Anthony <anthony_cristhian7@hotmail.com>
2025-02-22 11:17:49 -0500

    feat(task-card): develop task card component for individual task display

---
+++ frontend/src/app/components/task-card/task-card.component.ts
@@ -0,0 +1,30 @@
+import { DatePipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Tasks } from '@interfaces/tasks.interface';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-task-card',
+  standalone: true,
+  imports: [DatePipe, LucideAngularModule],
+  templateUrl: './task-card.component.html',
+})
+export class TaskCardComponent {
+  @Input() task!: Tasks;
+  @Output() updateStatus = new EventEmitter<{ id: number; status: string }>();
+  @Output() deleteTask = new EventEmitter<number>();
+  @Output() updateStatusClick = new EventEmitter<number>();
+
+  onUpdateStatus(status: string) {
+    this.updateStatus.emit({ id: this.task.id, status });
+  }
+
+  onPlayClick() { // <- Nueva función para actualizar el estado
+    this.updateStatusClick.emit(this.task.id);
+  }
+
+  onDeleteTask() {
+    this.deleteTask.emit(this.task.id);
+  }
+
+}
