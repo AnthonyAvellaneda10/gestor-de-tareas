b7986d243850954a855be62e18ae603f34224a2b
Anthony <anthony_cristhian7@hotmail.com>
2025-02-22 19:57:07 -0500

    refactor(backend/services): update tasks.service for improved functionality

--- backend/src/services/tasks.service.ts
+++ backend/src/services/tasks.service.ts
@@ -2,11 +2,11 @@ import { ITasksRepository } from "../interfaces/ITasksRepository";
 import { Task } from "../models/task.model";
 
 export class TasksService {
-    constructor(private tasksRepository: ITasksRepository) {}
+    constructor(private tasksRepository: ITasksRepository) { }
 
     async createTask(task: Task): Promise<Task> {
         const nowPeru = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Lima" }));
-    
+
         let dueDate: Date;
         if (task.due_datetime.toString().endsWith("Z")) {
             const parsedUTC = new Date(task.due_datetime);
@@ -14,19 +14,22 @@ export class TasksService {
         } else {
             dueDate = new Date(task.due_datetime);
         }
-            
+
         if (dueDate.getTime() < nowPeru.getTime() + 3600000) {
-             throw new Error("La fecha de vencimiento debe ser al menos 1 hora en el futuro");
+            throw new Error("La fecha de vencimiento debe ser al menos 1 hora en el futuro");
         }
-    
+
         task.status_id = task.status_id || 1;
         return this.tasksRepository.createTask(task);
-    }        
+    }
 
     async getTasks(): Promise<Task[]> {
+        // Primero obtenemos las tareas y actualizamos las vencidas
         const tasks = await this.tasksRepository.getTasks();
         await this.updateTaskStatusForOverdueTasks(tasks);
-        return tasks;
+
+        // Luego volvemos a consultar para obtener la lista con los estados actualizados
+        return await this.tasksRepository.getTasks();
     }
 
     async updateTaskStatus(taskId: number): Promise<Task> {
@@ -57,7 +60,7 @@ export class TasksService {
     private async updateTaskStatusForOverdueTasks(tasks: Task[]): Promise<void> {
         const now = new Date();
         for (const task of tasks) {
-            if (task.due_datetime && task.due_datetime < now && (task.status_id === 1 || task.status_id === 2)) {
+            if (task.due_datetime && (new Date(task.due_datetime).getTime() + 60000) < now.getTime() && (task.status_id === 1 || task.status_id === 2)) {
                 await this.tasksRepository.updateTaskStatus(task.id!, 4);
             }
         }
